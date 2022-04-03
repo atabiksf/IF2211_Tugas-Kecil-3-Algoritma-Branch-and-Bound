3dcf7a2bf525ed445e9556db424594435cae50e2
atabiksf <atabik.shofi@gmail.com>
2022-04-03 17:17:45 +0700

    add Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        PuzzleMatrix init = new PuzzleMatrix();
+        init.readFile();
+        init.printMatrix();
+        if(init.canSolved()){
+            long startTime = System.currentTimeMillis();
+            PuzzleTree tree = new PuzzleTree();
+            tree.puzzleTree.add(init);
+            tree.generateChilds(init);
+            tree.PuzzleSolving();
+            long endTime = System.currentTimeMillis();
+            for(PuzzleMatrix matrix : tree.solutionList){
+                System.out.println("Proses: ");
+                matrix.printMatrix();
+            }
+            System.out.println("Simpul yang dibangkitkan : " + tree.simplices);
+            long totalTime = endTime - startTime;
+            System.out.println("Total time : " + totalTime + " ms");
+        }
+    }
+}
